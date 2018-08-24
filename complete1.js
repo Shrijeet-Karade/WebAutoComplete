a8019979e2fd56983b3250e2e0e82f4ecad621fb
Shrijeet Karade <skarade@tavisca.com>
2018-08-24 11:44:50 +0530

    Final Submission

--- complete1.js
+++ complete1.js
@@ -10,10 +10,10 @@ function AutocompleteFunct(name,event)
   myArray.sort();
   if(name.length !== 0)
   { 
-    var count=0
+    var currentCountOfElementsInList=0
     var flag=0;
 
-     document.getElementById("list").innerHTML ='';
+    document.getElementById("list").innerHTML ='';
     var listOfAutocomplete = document.getElementById("list");
     for (var index = 0; index < myArray.length; index++)
     {
@@ -26,7 +26,7 @@ function AutocompleteFunct(name,event)
         newElement.setAttribute("onclick", "renderVisible('"+myArray[index]+"')");
         listOfAutocomplete.appendChild(newElement);
         newElement.innerHTML = myArray[index];
-        count++;
+        currentCountOfElementsInList++;
         flag=1;
       }
     }
@@ -43,61 +43,90 @@ function AutocompleteFunct(name,event)
   {
     document.getElementById("list").innerHTML ='';
   }
-
   console.log("exiting AutocompleteFunct and calling navigation function ");
-  navigateScroll(event,count);
+  navigateScroll(event,currentCountOfElementsInList);
 }
-function navigateScroll(event,count)
+function navigateScroll(event,currentCountOfElementsInList)
 {
-  var permanentCount= count;
-  if(permanentCount!=count)
-  {// this is if the number of elements in lists change . Then counter should initiate from start
+  //var lastKeyStroke= 0;
+  var previousCount= currentCountOfElementsInList;
+  if(previousCount!=currentCountOfElementsInList)
+  {
+    // this is if the number of elements in lists change . Then counter should initiate from start
     counter=0;
   }
   var listOfAutocomplete=document.getElementById("list").innerHTML;
   var x = event.which || event.keyCode;
-// UNHighlight every element in list here 
-for(var i=0;i<count;i++)
+  // UNHighlight every element in list here 
+for(var i=0;i<currentCountOfElementsInList;i++)
 {
   document.getElementsByTagName("ul")[0].childNodes[i].setAttribute("class","unHighlight");
 }
   if(x==40)
-  {
-    if(counter<count)
+  { 
+    if(counter<currentCountOfElementsInList-1)
     {
-     counter++;
-//highlight elements here
-    document.getElementsByTagName("ul")[0].childNodes[counter].setAttribute("class","highlight")     
+      //highlight elements here
+     document.getElementsByTagName("ul")[0].childNodes[counter].setAttribute("class","highlight");   
+     var elmnt = document.getElementsByTagName("ul")[0].childNodes[counter];
+     elmnt.scrollIntoView(false);
+     //  if(lastKeyStroke==38)
+     //  {
+     //   counter+=2;
+     //   lastKeyStroke=x;
+     //  }
+     //  else{
+     //   counter++;
+     //   lastKeyStroke=x;
+     //  }
+     // }
+    counter++;
     }
     else
     {
-      counter=count;    
-    }
+      counter=currentCountOfElementsInList-1;  
+      document.getElementsByTagName("ul")[0].childNodes[counter].setAttribute("class","highlight");  
+      var elmnt = document.getElementsByTagName("ul")[0].childNodes[counter];
+      elmnt.scrollIntoView(false);
+    }   
   }
   else if(x==38)
   {
-    if(counter>count)
+
+    if(counter>0)
     {
+      // if(lastKeyStroke==40)
+      // {
+      // counter-=2;
+      // lastKeyStroke=x;
+      // }
+      // else
+      // {
+      //   counter--;
+      //   lastKeyStroke=x;
+      // }
       counter--;
-document.getElementsByTagName("ul")[0].childNodes[counter].setAttribute("class","highlight")
-// highlight elements here  
+      document.getElementsByTagName("ul")[0].childNodes[counter].setAttribute("class","highlight");
+      var elmnt = document.getElementsByTagName("ul")[0].childNodes[counter];
+      elmnt.scrollIntoView(false);
+      // highlight elements here       
     }
     else
-    {
+    { 
       counter=0;
+      document.getElementsByTagName("ul")[0].childNodes[0].setAttribute("class","highlight");
+      var elmnt = document.getElementsByTagName("ul")[0].childNodes[counter];
+      elmnt.scrollIntoView(false);
     }
   }
   else if (x==13)
   {
-
       document.getElementById("enter-val").value=document.getElementsByTagName("ul")[0].childNodes[counter].innerHTML;
       document.getElementById("list").innerHTML="";
       //enter the value into the box
   }
   else{}
-
 }
-
 function renderVisible(name) 
 {
    document.getElementById("enter-val").value = name;
